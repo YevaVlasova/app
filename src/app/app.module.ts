6b44fbcb4479f0275334c1456bdaf240a2a616fe
YevaVlasova <eva-krechetova@ukr.net>
2023-04-11 19:00:34 +0300

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CoreModule } from 'src/app/components/core.module';
+import { PointsComponent } from './points/points.component';
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NewsComponent,
+    TasksComponent,
+    PointsComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class HomeModule { }
